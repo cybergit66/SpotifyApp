0d30bb05116f180c39bbd27eed55cd903891cbaa
Terence Highsmith <terence.highsmith@gmail.com>
2017-03-28 17:30:32 -0500

    added spotify search bar with axios functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar/SearchBar';
+import searchSpotify from '../utils/searchSpotify';
 
 export default class App extends Component {
 
@@ -6,13 +8,18 @@ export default class App extends Component {
     super(props);
     this.state = {
       initialMessage: 'greeting',
+      song: '',
     };
   }
-
+  fetchSongs = () => {
+    searchSpotify(this.state.song)
+      .then((tracks) => console.log('tracks', tracks));
+  }
   render() {
     const { initialMessage } = this.state;
     return (
       <div>
+        <SearchBar updateText={(song) => this.setState({ song: song })} fetchSongs={this.fetchSongs} />
         {initialMessage}
       </div>
     );
